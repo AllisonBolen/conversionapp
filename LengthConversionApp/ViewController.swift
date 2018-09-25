48555f8310529b40a90976f99ce1d597a9c589e5
Brendan Nahed <brendannahed@Brendans-MacBook-Pro-2.local>
2018-09-25 01:25:11 -0400

    I believe it is fully functioning

--- LengthConversionApp/ViewController.swift
+++ LengthConversionApp/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController, SettingsViewControllerDelegate {
     
     @IBOutlet weak var ToTextBox: UITextField!
     var currentMode = CalculatorMode.Length
+    var previousFromLabel: String = "Gallons"
+    var previousToLabel: String = "Liters"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,17 +46,23 @@ class ViewController: UIViewController, SettingsViewControllerDelegate {
     
     @IBAction func mode(_ sender: UIButton) {
         // modes = convert to volume
+        let tempFrom:String = self.FromLabel.text!
+        let tempTo:String = self.ToLabel.text!
         if (self.currentMode.rawValue == "Length"){
             self.titleLabel.text = "Volume Conversion"
-            self.FromLabel.text = "Gallons"
-            self.ToLabel.text = "Liters"
-            self.FromTextBox.placeholder = "Enter Volume in  \(FromLabel.text!)"
+            self.FromLabel.text = self.previousFromLabel
+            self.ToLabel.text = self.previousToLabel
+            self.previousFromLabel = tempFrom
+            self.previousToLabel = tempTo
+            self.FromTextBox.placeholder = "Enter Volume in \(FromLabel.text!)"
             self.ToTextBox.placeholder = "Enter Volume in \(ToLabel.text!)"
             self.currentMode = CalculatorMode.Volume
         }else{
             self.titleLabel.text = "Length Conversion"
-            self.FromLabel.text = "Yards"
-            self.ToLabel.text = "Meters"
+            self.FromLabel.text = self.previousFromLabel
+            self.ToLabel.text = self.previousToLabel
+            self.previousFromLabel = tempFrom
+            self.previousToLabel = tempTo
             self.FromTextBox.placeholder = "Enter Length in \(FromLabel.text!)"
             self.ToTextBox.placeholder = "Enter Length in \(ToLabel.text!)"
             self.currentMode = CalculatorMode.Length
@@ -82,28 +90,79 @@ class ViewController: UIViewController, SettingsViewControllerDelegate {
     
     @IBAction func Calculate(_ sender: UIButton) {
 
-        if let yd:Double = Double(self.FromTextBox.text!)  {
+        if let to:Double = Double(self.FromTextBox.text!)  {
             FromTextBox.resignFirstResponder()
             dismissKeyboard()
-            ToTextBox.text = String(format: "%f",yd*lengthConversionTable[LengthConversionKey(toUnits: .Meters, fromUnits: .Yards)]!)
-            
+            ToTextBox.text = String(format: "%f",to*conversionFinder())
         }
-        else if let mt:Double = Double(self.ToTextBox.text!) {
+        else if let from:Double = Double(self.ToTextBox.text!) {
             ToTextBox.resignFirstResponder()
             dismissKeyboard()
-            FromTextBox.text = String(format: "%f", mt*lengthConversionTable[LengthConversionKey(toUnits: .Yards, fromUnits: .Meters)]!)
+            FromTextBox.text = String(format: "%f", from/conversionFinder())
         }
+    }
+    
+    func conversionFinder() -> Double{
+        if currentMode.rawValue == "Length"{
+            if FromLabel.text == "Meters" && ToLabel.text == "Yards"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Yards, fromUnits: .Meters)]!
+            }else if FromLabel.text == "Meters" && ToLabel.text == "Meters"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Meters, fromUnits: .Meters)]!
+            }else if FromLabel.text == "Meters" && ToLabel.text == "Miles"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Miles, fromUnits: .Meters)]!
+            }else if FromLabel.text == "Yards" && ToLabel.text == "Meters"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Meters, fromUnits: .Yards)]!
+            }else if FromLabel.text == "Yards" && ToLabel.text == "Yards"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Yards, fromUnits: .Yards)]!
+            }else if FromLabel.text == "Yards" && ToLabel.text == "Miles"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Miles, fromUnits: .Yards)]!
+            }else if FromLabel.text == "Miles" && ToLabel.text == "Miles"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Miles, fromUnits: .Miles)]!
+            }else if FromLabel.text == "Miles" && ToLabel.text == "Meters"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Meters, fromUnits: .Miles)]!
+            }else if FromLabel.text == "Miles" && ToLabel.text == "Yards"{
+                return lengthConversionTable[LengthConversionKey(toUnits: .Yards, fromUnits: .Miles)]!
+            }
+        }else{
+            if FromLabel.text == "Gallons" && ToLabel.text == "Liters"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Liters, fromUnits: .Gallons)]!
+            } else if FromLabel.text == "Gallons" && ToLabel.text == "Gallons"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Gallons, fromUnits: .Gallons)]!
+            }else if FromLabel.text == "Gallons" && ToLabel.text == "Quarts"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Quarts, fromUnits: .Gallons)]!
+            } else if FromLabel.text == "Liters" && ToLabel.text == "Gallons"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Gallons, fromUnits: .Liters)]!
+            } else if FromLabel.text == "Liters" && ToLabel.text == "Liters"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Liters, fromUnits: .Liters)]!
+            } else if FromLabel.text == "Liters" && ToLabel.text == "Quarts"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Quarts, fromUnits: .Liters)]!
+            } else if FromLabel.text == "Quarts" && ToLabel.text == "Gallons"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Gallons, fromUnits: .Quarts)]!
+            } else if FromLabel.text == "Quarts" && ToLabel.text == "Quarts"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Quarts, fromUnits: .Quarts)]!
+            } else if FromLabel.text == "Quarts" && ToLabel.text == "Liters"{
+                return volumeConversionTable[VolumeConversionKey(toUnits: .Liters, fromUnits: .Quarts)]!
+            }
+        }
+        return 0.0
     }
     
     func indicateSelection(vice_1: String, vice_2: String){
         self.FromLabel.text = vice_1
         self.ToLabel.text = vice_2
+        if (self.currentMode.rawValue == "Length"){
+            self.FromTextBox.placeholder = "Enter Length in \(FromLabel.text!)"
+            self.ToTextBox.placeholder = "Enter Length in \(ToLabel.text!)"
+        }else{
+            self.FromTextBox.placeholder = "Enter Volume in  \(FromLabel.text!)"
+            self.ToTextBox.placeholder = "Enter Volume in \(ToLabel.text!)"
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let dest = segue.destination.childViewControllers[0] as? SettingsViewController{
-            dest.to_selection = self.FromLabel.text!
-            dest.from_selection = self.ToLabel.text!
+            dest.to_selection = self.ToLabel.text!
+            dest.from_selection = self.FromLabel.text!
             dest.currentMode = self.currentMode
             dest.delegate = self
         }
